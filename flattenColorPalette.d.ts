2027cdf3733c1104f95c4ce2de0c74ea6f85d986
ZaneFerns360 <zane.ferns360@gmail.com>
2024-03-15 09:29:09 +0530

    acernity added

---
+++ flattenColorPalette.d.ts
@@ -0,0 +1,4 @@
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  const flattenColorPalette: any; // Adjust the type as needed
+  export default flattenColorPalette;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    // rest of the code
+    extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
