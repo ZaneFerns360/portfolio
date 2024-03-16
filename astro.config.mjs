b417f08cdc745495bbd755d7a76fc72e5bf6c7c3
ZaneFerns360 <zane.ferns360@gmail.com>
2024-03-16 16:23:43 +0530

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro",
+        autoInstall: true,
+      }),
+    ],
+  },
 });
